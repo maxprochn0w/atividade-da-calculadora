feda6efaaca9ca6738e62c84a6af0eead801860b
maxprochn0w <161316213+maxprochn0w@users.noreply.github.com>
2024-04-22 09:19:46 -0300

    feita a calculation

---
+++ pastacalculation/atividadejs.js
@@ -0,0 +1,29 @@
+function calculate() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let operator = document.getElementById('operator').value;
+    let result;
+  
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2 !== 0) {
+          result = num1 / num2;
+        } else {
+          result = 'Divisão por zero';
+        }
+        break;
+      default:
+        result = 'Operador inválido';
+    }
+  
+    document.getElementById('result').value = result;
+  }
